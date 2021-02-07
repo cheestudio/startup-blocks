6f8420c0f8e6bdcb8163bc5e5785d13c6e79ec5c
unknown <lars@goodchee.com>
2021-02-07 15:58:21 -0700

    various cleanup

--- page.php
+++ page.php
@@ -1,19 +1,9 @@
 <?php get_header(); ?>
 
-<?= get_field('title1');?>
-
 <?php if ( have_posts() ) : ?>
-  <div class="default-page">
-    <div class="container">
-
-      <div class="default-page--content">
-        <?php while ( have_posts() ) : the_post(); ?>
-          <?php the_content();?>
-        <?php endwhile; ?>
-      </div>
-
-    </div>
-  </div>
+	<?php while ( have_posts() ) : the_post(); ?>
+		<?php the_content();?>
+	<?php endwhile; ?>
 <?php endif; ?>
 
 
