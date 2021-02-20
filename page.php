18df4cfcb32a628c1dcfac937a69c772dde38292
unknown <lars@goodchee.com>
2021-02-20 14:03:04 -0700

    block width fixes

--- page.php
+++ page.php
@@ -1,10 +1,11 @@
 <?php get_header(); ?>
 
-<?php if ( have_posts() ) : ?>
-	<?php while ( have_posts() ) : the_post(); ?>
-		<?php the_content();?>
-	<?php endwhile; ?>
-<?php endif; ?>
-
+<div class="content-blocks">
+	<?php if ( have_posts() ) : ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<?php the_content();?>
+		<?php endwhile; ?>
+	<?php endif; ?>
+</div>
 
 <?php get_footer(); ?>
