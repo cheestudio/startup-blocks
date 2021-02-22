6ff96eef835d965737b22119afbeeb6c03701d81
unknown <lars@goodchee.com>
2021-02-22 15:45:22 -0700

    custom colors for all editors (block,tinymce,acf)

--- init/helpers.php
+++ init/helpers.php
@@ -270,23 +270,30 @@ function my_acf_admin_head() {
   <?php
 }
 
-/* Block Editor Colors
+/* Block/TinyMCE/ACF Editor Colors
 ========================================================= */
+
+/* Global Color Definitions */
+
+$color_vars = array(
+  'Black' => '000000',
+  'White' => 'ffffff'
+);
+
+/* Block Editor Colors 
+-----------------------------------*/
 
 // Disable Custom Colors (optional)
 //add_theme_support( 'disable-custom-colors' );
-
+//
 function block_editor_color_map() {
-  $colors_vars = array(
-    'Black' => '#000',
-    'White' => '#fff'
-  );
+  global $color_vars;
   $colors = array();
-  foreach ($colors_vars as $name => $hex) {
+  foreach ($color_vars as $name => $hex) {
    $colors[] = array(
     'name'  => __( $name ),
     'slug'  => strtolower($name),
-    'color' => $hex,
+    'color' => '#'.$hex,
   );
  }
  return $colors;
@@ -297,6 +304,60 @@ function custom_block_editor_colors(){
   add_theme_support( 'editor-color-palette', $colors );
 }
 add_action( 'after_setup_theme', 'custom_block_editor_colors' );
+
+
+/* Branded Color Picker for ACF 
+-----------------------------------*/
+
+function output_the_colors() {
+
+  $color_palette = current( (array) get_theme_support( 'editor-color-palette' ) ); // use defined Block Editor colors
+
+  if ( !$color_palette )
+    return;
+
+  ob_start();
+
+  echo '[';
+  foreach ( $color_palette as $color ) {
+    echo "'" . $color['color'] . "', ";
+  }
+  echo ']';
+
+  return ob_get_clean();
+}
+
+function custom_acf_colors() { 
+  $custom_colors = output_the_colors();
+  ?>
+  <script type="text/javascript">
+    (function($) {
+     acf.add_filter('color_picker_args', function( args, $field ){
+      args.palettes = <?php echo $custom_colors; ?>
+      // return colors
+      return args;
+               });
+   })(jQuery);
+ </script>
+<?php }
+add_action('acf/input/admin_footer', 'custom_acf_colors');
+
+/* Add Custom Colors to ACF Wizzy  
+-----------------------------------*/
+
+function custom_tinymce_colors( $init ) {
+  global $color_vars;
+  $colors_custom = array();
+  foreach ( $color_vars as $name => $hex ) {
+    $colors_custom[] = $hex;
+    $colors_custom[] = $name;
+  }
+  $colors = $colors_custom;
+  $init['textcolor_map']  = json_encode($colors);
+  return $init;
+}
+add_filter( 'tiny_mce_before_init', 'custom_tinymce_colors' );
+
 
 /* ACF Block Class/ID
 ========================================================= */
