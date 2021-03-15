bc584c06ee9af231a1d54e388abb69ffaec6d790
Matt <matt@goodchee.com>
2021-03-15 14:23:14 -0700

    empty out footer

--- footer.php
+++ footer.php
@@ -1,9 +1,5 @@
 <?php // VARs & optional ACF code
 $logo        = get_template_directory_uri() . '/assets/img/svg/logo.svg';
-$phone       = get_field('company_phone', 'option');
-$email       = get_field('company_email', 'option');
-$address     = get_field('company_address', 'option');
-$copyright   = get_field('company_copyright', 'option');
 $footer_code = get_field('footer_code', 'option'); ?>
 
 </main>
@@ -25,26 +21,6 @@ $footer_code = get_field('footer_code', 'option'); ?>
 
     <div class="footer--social">
       <?php include( locate_template('partials/elements/social-icons.php') ); ?>
-    </div>
-
-    <div class="footer--contact">
-      <?php if ( $address ) : ?>
-        <div class="address"><?= $address; ?></div>
-      <?php endif; ?>
-      <?php if ( $phone ) : ?>
-        <p class="phone">
-          <a href="tel:+1-<?= sanitize_title_with_dashes( $phone ); ?>" title="Link to Call Us"><?= $phone; ?></a>
-        </p>
-      <?php endif; ?>
-      <?php if ( $email ) : ?>
-        <p class="email">
-          <a href="mailto:<?= $email; ?>" title="Link to Email Us"><?= $email; ?></a>
-        </p>
-      <?php endif; ?>
-    </div>
-
-    <div class="footer--copyright">
-      <?= $copyright; ?>
     </div>
 
   </div>
