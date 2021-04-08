b14b4d03f6b942a3faee62a5ec6675503627d71b
unknown <lars@goodchee.com>
2021-04-08 13:17:30 -0700

    adding block editor functions

--- functions.php
+++ functions.php
@@ -9,3 +9,4 @@ require_once locate_template('/init/helpers.php');   // Other default theme func
 require_once locate_template('/init/custom.php');   // Custom function additions
 require_once locate_template('/init/cpt.php');     // Custom Post Types
 require_once locate_template('/init/acf.php');     // Advanced Custom Fields
+require_once locate_template('/init/block-editor.php');     // Custom Block Editor Functions
---
+++ init/block-editor.php
@@ -0,0 +1,60 @@
+<?php
+/* BLOCK EDITOR FUNCTIONS
+========================================================= */
+
+/* Disable Gutenberg Per Post Type
+========================================================= */
+add_filter('use_block_editor_for_post_type', 'prefix_disable_gutenberg', 10, 2);
+function prefix_disable_gutenberg($current_status, $post_type)
+{
+  if ($post_type === 'CPT_HERE' || $post_type === 'ANOTHER_CPT_HERE') return false;
+  return $current_status;
+}
+
+/* Templates and Page IDs without editor
+========================================================= */
+
+function chee_disable_editor( $id = false ) {
+
+  $excluded_templates = array(
+    //'templates/tpl-{template-name}.php',
+  );
+
+  if( empty( $id ) )
+    return false;
+
+  $id = intval( $id );
+  $template = get_page_template_slug( $id );
+
+  return in_array( $template, $excluded_templates );
+}
+
+function chee_disable_gutenberg( $can_edit, $post_type ) {
+
+  if( ! ( is_admin() && !empty( $_GET['post'] ) ) )
+    return $can_edit;
+
+  if( chee_disable_editor( $_GET['post'] ) )
+    $can_edit = false;
+
+  return $can_edit;
+
+}
+add_filter( 'gutenberg_can_edit_post_type', 'chee_disable_gutenberg', 10, 2 );
+add_filter( 'use_block_editor_for_post_type', 'chee_disable_gutenberg', 10, 2 );
+
+/* Disable Classic Editor Sidewide
+========================================================= */
+
+/*function chee_disable_classic_editor() {
+
+  $screen = get_current_screen();
+  if( 'page' !== $screen->id || ! isset( $_GET['post']) )
+    return;
+
+  if( chee_disable_editor( $_GET['post'] ) ) {
+    remove_post_type_support( 'page', 'editor' );
+  }
+
+}
+add_action( 'admin_head', 'chee_disable_classic_editor' );*/
