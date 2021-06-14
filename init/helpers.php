fbb7d400d096de5caebcf19aa4677387750d8613
Lars Faye <lars@goodchee.com>
2021-06-14 09:53:31 -0700

    updating gravity form buttons markup function

--- init/helpers.php
+++ init/helpers.php
@@ -96,12 +96,32 @@ function svg($path, $alt='')
 
 /* Gravity Forms Button Markup
 ========================================================= */
-add_filter('gform_submit_button', 'form_submit_button', 10, 2);
-function form_submit_button($button, $form)
-{
-    return "<button class='button gform_button' id='gform_submit_button_{$form['id']}'>{$form['button']['text']}</button>";
+/**
+ * Filters the next, previous and submit buttons.
+ * Replaces the forms <input> buttons with <button> while maintaining attributes from original <input>.
+ *
+ * @param string $button Contains the <input> tag to be filtered.
+ * @param object $form Contains all the properties of the current form.
+ *
+ * @return string The filtered button.
+ */
+add_filter( 'gform_next_button', 'input_to_button', 10, 2 );
+add_filter( 'gform_previous_button', 'input_to_button', 10, 2 );
+add_filter( 'gform_submit_button', 'input_to_button', 10, 2 );
+function input_to_button( $button, $form ) {
+    $dom = new DOMDocument();
+    $dom->loadHTML( '<?xml encoding="utf-8" ?>' . $button );
+    $input = $dom->getElementsByTagName( 'input' )->item(0);
+    $new_button = $dom->createElement( 'button' );
+    $new_button->appendChild( $dom->createTextNode( $input->getAttribute( 'value' ) ) );
+    $input->removeAttribute( 'value' );
+    foreach( $input->attributes as $attribute ) {
+        $new_button->setAttribute( $attribute->name, $attribute->value );
+    }
+    $input->parentNode->replaceChild( $new_button, $input );
+ 
+    return $dom->saveHtml( $new_button );
 }
-
 
 /* Gravity Forms anchor creation
 ========================================================= */
