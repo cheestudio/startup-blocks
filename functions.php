7f54d05b5e363dbea544e9274a8331a9673dd2bc
Lars Faye <lars@goodchee.com>
2021-06-24 22:12:11 -0700

    adding color functions include

--- functions.php
+++ functions.php
@@ -8,5 +8,6 @@ require_once locate_template('/init/scripts.php');    // Theme Scripts and Style
 require_once locate_template('/init/helpers.php');   // Other default theme functions
 require_once locate_template('/init/custom.php');   // Custom function additions
 require_once locate_template('/init/cpt.php');     // Custom Post Types
+require_once locate_template('/init/colors.php');     // Color Functions
 require_once locate_template('/init/acf.php');     // Advanced Custom Fields
 require_once locate_template('/init/block-editor.php');     // Custom Block Editor Functions
