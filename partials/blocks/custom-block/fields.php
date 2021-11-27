bb919439ee5f77cd993e05b530057ee15c8001ce
Lars Faye <lars@goodchee.com>
2021-11-27 10:35:20 -0500

    updating comments for block field init

--- partials/blocks/custom-block/fields.php
+++ partials/blocks/custom-block/fields.php
@@ -1,19 +1,19 @@
 <?php
+/* Block Field Init */
 use StoutLogic\AcfBuilder\FieldsBuilder;
-
 $path = basename(__DIR__);
 $name = str_replace('-', '_', $path);
-$group_name = $name.'_group';
+$group_name = $name . '_group';
 $group_label = str_replace('-', ' ', $path);
-
 $name = new FieldsBuilder($name);
+/* END Block Field Init */
 
 $name
 ->addGroup($group_name, [
 	'label' => ucwords($group_label).' Block',
 	'instructions' => '',
 	'layout' => 'block',
-]) //REQUIRED
+]) //REQUIRED GROUP
 
 
 
@@ -23,7 +23,7 @@ https://github.com/tdwesten/acf-fieldattributes-cheatsheet/blob/master/acf-field
 
 
 
-->endGroup() // REQUIRED
+->endGroup() // END REQUIRED GROUP
 ->setLocation('block', '==', 'acf/'.$path);
 
 return $name;
