ea1093e2edfe94f3cf08dd497302bb0fb0afed71
Lars Faye <lars@goodchee.com>
2021-11-27 11:13:04 -0500

    updating fontawesome kit

--- init/fonts.php
+++ init/fonts.php
@@ -4,7 +4,7 @@
 
 function chee_enqueue_custom_fonts() {
 	// wp_enqueue_style('typekit', "https://use.typekit.net/rfw6ied.css" );
-	// wp_enqueue_script('fontawesome', "https://use.fontawesome.com/430a4cedb2.js" );
+	// wp_enqueue_script('fontawesome', "https://kit.fontawesome.com/78f93195e3.js" );
 	// wp_enqueue_style('google', "https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Lato:wght@400;700&display=swap", array(), null); NOTE: Google Fonts require additional arguments, as listed here
 }
 add_action('admin_enqueue_scripts', 'chee_enqueue_custom_fonts');
