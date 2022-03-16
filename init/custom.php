1db5cd5ba35a9c2bc151152bb384e8d8caca81f0
Matt <matt@goodchee.com>
2022-03-16 10:01:17 -0700

    default sort pages by title

--- init/custom.php
+++ init/custom.php
@@ -1,3 +1,16 @@
 <?php 
 /* Custom Functions
 ========================================================= */
+
+/* Sort Post Type by Title
+========================================================= */
+add_filter('pre_get_posts', 'custom_order_post_type');
+function custom_order_post_type( $query ) {
+  if ( $query->is_admin ) {
+    if ( $query->get('post_type') == 'page' ) {
+      $query->set('orderby', 'title');
+      $query->set('order', 'ASC');
+    }
+  }
+  return $query;
+}
