9696e5315565157405e52aaca78437a92f10136a
Matt <matt@goodchee.com>
2022-08-18 17:30:56 -0700

    add accordion block

---
+++ partials/blocks/accordion-section/template.php
@@ -0,0 +1,46 @@
+<?php
+/*
+Block Name: Accordion / FAQs
+Block Description: Unlimited accordion items w title & text content
+Block Category: Custom {CLIENT} Blocks
+Block Align: full
+Block Icon: plus-alt
+Post Types: post, page
+*/
+
+// Block Image Preview
+if ( isset($block['data']['block_preview']) ) :
+  block_preview(__FILE__);
+
+// Block Content
+else :
+  $group = blockFieldGroup(__FILE__); // REQUIRED
+  $heading = $group['heading'];
+  $sections = $group['sections_rep'];
+  ?>
+
+  <?php if ( $sections ) : $i = 0; ?>
+    <section <?php block_class_id( $block,'accordion-row' ); ?>>
+      <div class="wrapper">
+        
+        <?php if ( !empty($heading) ) echo "<div class='accordion-row--heading'><h2>{$heading}</h2></div>"; ?>
+
+        <div class="accordion-row--content">
+          <?php foreach ( $sections as $section ) : $i++; // title, content ?>
+            <div class="entry" id="entry-<?= $i; ?>">
+              <div class="toggle">
+                <a href="#" title="<?= $section['title']; ?>">
+                  <h3><?= $section['title']; ?></h3>
+                  <?php svg_inline('icon-plus.svg'); ?>
+                  <?php svg_inline('icon-minus.svg'); ?>
+                </a>
+              </div>
+              <div class="inner"><?= $section['content']; ?></div>
+            </div>
+          <?php endforeach; ?>
+        </div>
+      </div>
+    </section>
+  <?php endif; ?>
+
+<?php endif; ?>
