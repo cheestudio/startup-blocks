1173efd5717fcf52a6fc7c74d0c077cf43f3a470
Lars Faye <lars@goodchee.com>
2022-10-27 12:05:42 -0400

    sync ACF colors to theme.json

--- page.php
+++ page.php
@@ -3,12 +3,6 @@
   get_header();
 ?>
 
-<?php 
-$settings = get_default_block_editor_settings();
-?>
-<pre>
-	 <?php print_r($settings);?>
-</pre>
 <div class="content-blocks">
 	<?php if ( have_posts() ) : ?>
 		<?php while ( have_posts() ) : the_post(); ?>
