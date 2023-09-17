7cf10ee453134a2bde54f625fc016add32c9ef87
Lars Faye <lars@goodchee.com>
2023-09-17 15:47:19 -0400

    password protection for page content

--- page.php
+++ page.php
@@ -5,9 +5,16 @@
 
 
 <div class="content-blocks">
-  <?php if ( have_posts() ) : ?>
-    <?php while ( have_posts() ) : the_post(); ?>
-      <?php the_content(); ?>
+  <?php if (have_posts()) : ?>
+    <?php while (have_posts()) : the_post(); ?>
+      <?php if (post_password_required()) : ?>
+        <div class="password-protected-content">
+          <h1><?php the_title(); ?></h1>
+          <?= get_the_password_form(); ?>
+        </div>
+      <?php else : ?>
+        <?php the_content(); ?>
+      <?php endif; ?>
     <?php endwhile; ?>
   <?php endif; ?>
 </div>
