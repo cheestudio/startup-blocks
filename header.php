68ec5f40a42c2967222d1ea4cc2a22d6cfd95e12
Matt <matt@goodchee.com>
2023-10-24 16:35:59 -0700

    remove unused logo var

--- header.php
+++ header.php
@@ -7,33 +7,31 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="theme-color" content="#ffffff">
   <?php wp_head(); ?>
-  <link rel="alternate" type="application/rss+xml" title="<?= get_bloginfo('name'); ?> Feed"
-    href="<?= home_url(); ?>/feed/">
+  <link rel="alternate" type="application/rss+xml" title="<?= get_bloginfo('name'); ?> Feed" href="<?= home_url(); ?>/feed/">
 
   <?php // Logos & Optional ACF Header Code
   require_once locate_template('/init/shortcodes.php');
-  $logo = get_template_directory_uri() . '/assets/img/svg/logo.svg';
-  if ( function_exists('get_field') ) :
+  if (function_exists('get_field')) :
     $head_code = get_field('head_code', 'option');
     $body_code = get_field('body_code', 'option');
   endif;
-  if ( isset( $head_code ) ) echo $head_code; ?>
+  if (isset($head_code)) echo $head_code; ?>
 </head>
 
 <body <?php body_class(); ?> id="top-of-page">
 
   <!-- Accessibility Skip to Content -->
-  <a class="skip-link screen-reader-text" href="#top-of-content"><?php esc_html_e( 'Skip to content', 'chee' ); ?></a>
+  <a class="skip-link screen-reader-text" href="#top-of-content"><?php esc_html_e('Skip to content', 'chee'); ?></a>
 
   <?php // Optional ACF Body Code
-  if ( isset($body_code) ) echo $body_code; ?>
+  if (isset($body_code)) echo $body_code; ?>
 
   <header class="main-banner">
     <div class="container">
 
       <div class="logo-brand">
         <a href="<?= home_url(); ?>" class="brand" title="Link to Go Home">
-          <?php get_template_part('partials/elements/logo', null, [ 
+          <?php get_template_part('partials/elements/logo', null, [
             'location' => 'desktop',
           ]); ?>
         </a>
