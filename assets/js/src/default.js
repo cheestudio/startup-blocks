15c91c690a4554d776182463d1555fd4a884562d
Matt <matt@goodchee.com>
2023-10-24 16:31:49 -0700

    add no-scroll exclusion to smooth scroll script

--- assets/js/src/default.js
+++ assets/js/src/default.js
@@ -74,20 +74,22 @@ function debounce(func, delay) {
   $('a[href^="#"]')
     .not('[href="#"]')
     .click(function (event) {
-      if (
-        location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
-        location.hostname == this.hostname
-      ) {
-        var target = $(this.hash);
-        target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-        if (target.length) {
-          event.preventDefault();
-          $("html, body").animate(
-            {
-              scrollTop: target.offset().top,
-            },
-            800
-          );
+      if (!$(this).hasClass("no-scroll")) {
+        if (
+          location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
+          location.hostname == this.hostname
+        ) {
+          var target = $(this.hash);
+          target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+          if (target.length) {
+            event.preventDefault();
+            $("html, body").animate(
+              {
+                scrollTop: target.offset().top,
+              },
+              800
+            );
+          }
         }
       }
     });
