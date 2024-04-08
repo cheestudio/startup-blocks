bc412b93aee88788775d2ada3c0811e24754b47a
Matt <matt@goodchee.com>
2024-04-08 10:54:06 -0700

    comment out unused block editor functions

--- init/block-editor.php
+++ init/block-editor.php
@@ -9,41 +9,35 @@
 
 /* Disable Gutenberg Per Post Type
 ========================================================= */
-function prefix_disable_gutenberg($current_status, $post_type) {
-  if ($post_type === 'CPT_HERE' || $post_type === 'ANOTHER_CPT_HERE') return false;
-  return $current_status;
-}
-add_filter('use_block_editor_for_post_type', 'prefix_disable_gutenberg', 10, 2);
+// add_filter('use_block_editor_for_post_type', 'prefix_disable_gutenberg', 10, 2);
+// function prefix_disable_gutenberg($current_status, $post_type) {
+//   if ($post_type === 'CPT_HERE' || $post_type === 'ANOTHER_CPT_HERE') return false;
+//   return $current_status;
+// }
 
 
 /* Templates and Page IDs without editor
 ========================================================= */
-function chee_disable_editor( $id = false ) {
-  $excluded_templates = array(
-    //'templates/tpl-{template-name}.php',
-  );
+// add_filter( 'gutenberg_can_edit_post_type', 'chee_disable_gutenberg', 10, 2 );
+// add_filter( 'use_block_editor_for_post_type', 'chee_disable_gutenberg', 10, 2 );
+// function chee_disable_editor( $id = false ) {
+//   $excluded_templates = array(
+//     'templates/tpl-{template-name}.php',
+//   );
+//   if ( empty( $id ) )
+//     return false;
+//   $id = intval( $id );
+//   $template = get_page_template_slug( $id );
+//   return in_array( $template, $excluded_templates );
+// }
 
-  if ( empty( $id ) )
-    return false;
-
-  $id = intval( $id );
-  $template = get_page_template_slug( $id );
-
-  return in_array( $template, $excluded_templates );
-}
-
-function chee_disable_gutenberg( $can_edit, $post_type ) {
-  if ( ! ( is_admin() && !empty( $_GET['post'] ) ) )
-    return $can_edit;
-
-  if ( chee_disable_editor( $_GET['post'] ) )
-    $can_edit = false;
-
-  return $can_edit;
-
-}
-add_filter( 'gutenberg_can_edit_post_type', 'chee_disable_gutenberg', 10, 2 );
-add_filter( 'use_block_editor_for_post_type', 'chee_disable_gutenberg', 10, 2 );
+// function chee_disable_gutenberg( $can_edit, $post_type ) {
+//   if ( ! ( is_admin() && !empty( $_GET['post'] ) ) )
+//     return $can_edit;
+//   if ( chee_disable_editor( $_GET['post'] ) )
+//     $can_edit = false;
+//   return $can_edit;
+// }
 
 
 /* Disable Classic Editor Sidewide
@@ -64,6 +58,7 @@ add_action( 'admin_head', 'chee_disable_classic_editor' );*/
 
 /* Custom Block Categories
 ========================================================= */
+add_filter( 'block_categories_all', 'custom_block_categories', 10, 2 );
 function custom_block_categories( $categories ) {
   return array_merge(
     array(
@@ -76,28 +71,27 @@ function custom_block_categories( $categories ) {
     $categories
   );
 }
-add_filter( 'block_categories_all', 'custom_block_categories', 10, 2 );
 
 
 /* Patterns
 ========================================================= */
 function chee_patterns_init() {
+add_action('after_setup_theme', 'chee_patterns_init');
   remove_theme_support('core-block-patterns');
   register_block_pattern_category( 'chee', array('label' => __('Chee', 'chee')) );
 } 
-add_action('after_setup_theme', 'chee_patterns_init');
 
 
 /* Disable Template Editing
 ========================================================= */
 remove_theme_support( 'block-templates' );
 
+
 /* Disable Inline Styles
 ========================================================= */
-
 add_filter( 'styles_inline_size_limit', '__return_zero' );
+
 
 /* Conditional Block Assets (load only when used)
 ========================================================= */
-
 add_filter( 'should_load_separate_core_block_assets', '__return_true' );
