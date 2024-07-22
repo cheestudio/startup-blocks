2842497f107f133c6d0e0223b7e45254da023a36
Matthew James <matt@goodchee.com>
2024-07-22 13:00:28 -0700

    add optional display settings  to acf page template

--- init/fields/example-page-template.php
+++ init/fields/example-page-template.php
@@ -1,23 +1,30 @@
 <?php
+
 use StoutLogic\AcfBuilder\FieldsBuilder;
 
-$templates = new FieldsBuilder('template_example', ['style' => 'seamless']);
+$templates = new FieldsBuilder(
+  'template_example',
+  [
+    'style' => 'seamless', // or default
+    'position' => 'normal' // or acf_after_title, side
+  ]
+);
 
 $templates
-// ->setGroupConfig('hide_on_screen', [
-//   'the_content',
-//   'featured_image'
-// ])
-  
-->addGroup('template_example_group', [
-  'label'        => 'Group Field for Templates',
-  'instructions' => '',
-  'layout'       => 'block',
-])
+  // ->setGroupConfig('hide_on_screen', [
+  //   'the_content',
+  //   'featured_image'
+  // ])
 
-->endGroup()
+  ->addGroup('template_example_group', [
+    'label'        => 'Group Field for Templates',
+    'instructions' => '',
+    'layout'       => 'block',
+  ])
 
-->setLocation('post_type', '==', 'page')
-->and('page_template', '==', 'templates/tpl-DEFAULT.php');
+  ->endGroup()
+
+  ->setLocation('post_type', '==', 'page')
+  ->and('page_template', '==', 'templates/tpl-DEFAULT.php');
 
 return $templates;
